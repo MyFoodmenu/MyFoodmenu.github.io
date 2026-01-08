7de1486a55e9b50df584c7ef088952d19a0cf848
MyFoodmenu <hilmanawang33@gmail.com>
2026-01-08 23:38:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ⬅️ OTOMATIS cocok untuk GitHub Pages
   server: {
     host: "::",
     port: 8080,
